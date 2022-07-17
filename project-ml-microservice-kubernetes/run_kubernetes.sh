0f20ba613f0e8e872e7468ad59710a973de57737
EC2 Default User <ec2-user@ip-172-31-19-129.ec2.internal>
2022-07-17 11:33:46 +0000

    config file added

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="garimamodi/project4_udacity"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run development --image=garimamodi/project4_udacity:latest --port=8000
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/development 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,17 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+#dockerpath="garimamodi/project4_udacityapi"
+dockerpath="garimamodi/project4_udacity"
+
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $dockerpath" 
+docker login
+docker tag 8ef3cf703e46 garimamodi/project4_udacity:latest
 
 # Step 3:
 # Push image to a docker repository
+#docker push <hostname>:<repository-port>/<image>:<tag>
+docker push garimamodi/project4_udacity:latest
